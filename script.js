b0853df2fd22502d5ee6c484c79c525264f17e1d
Awhit-commits <ndrw_whtmr@ymail.com>
2020-03-06 13:35:27 -0600

    added project minus CSS styling

---
+++ script.js
@@ -0,0 +1,58 @@
+let printEntries = (new_list) => {
+    previousEntries = document.getElementById("previously")
+    let sum = 0;
+    new_list.forEach(function(currentValue,index){
+        sum+= currentValue;
+        previousEntries.innerHTML =`<p> ${currentValue.date_created}</p>`;
+
+    });
+    
+}
+
+
+//Function for creating object instances of journal data
+let createJournal = (date, title, message, confidence,list) => {
+    // let journalEntries =[];
+    const journalEntry = {
+        date_created: date,
+        reflectTitle: title,
+        reflect_message: message,
+        reflect_confidence: confidence,
+    }
+    //Pushing everything to a list
+
+
+    // console.log(journalEntries);
+    list.push(journalEntry);
+    //more debugging statements
+    console.log(list);
+
+    //Sending list to print to DOM
+    printEntries (list);
+
+}
+let main = () => {
+    let journalEntries = [];
+    reflectDate = document.getElementById("date");
+    reflectTitle = document.getElementById("title");
+    reflectMessage = document.getElementById("reflection");
+    reflectConfidence = document.getElementById("reflection_confidence");
+    submitBtn = document.getElementById("submit");
+    submitBtn.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        console.log(reflectDate.value);
+        console.log(reflectTitle.value);
+        console.log(reflectConfidence.value);
+        dateValue = reflectDate.value;
+        titleValue = reflectTitle.value;
+        messageValue = reflectMessage.value;
+        confidenceValue = reflectConfidence.value;
+        createJournal(dateValue, titleValue, messageValue, confidenceValue,journalEntries);
+        // evt.preventDefault();
+    })
+
+
+
+
+}
+main();
