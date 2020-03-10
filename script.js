728217b44bf9e37f46f24d95ef9d6c6ae1e25aa8
Awhit-commits <ndrw_whtmr@ymail.com>
2020-03-10 04:28:21 -0500

    Fixed the appendChild element and Fixed the grid display

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let printEntries = (new_list) => {
     let previousEntries = document.getElementById("previously")
-    let entryBlockDisplay ="";
+    // let entryBlockDisplay ="";
+    let p = document.createElement("p");
+    
      // let sum = 0;
-    // new_list.forEach(function(currentValue,index){
+    new_list.forEach(function(currentValue,index){
     //     let p = document.createElement("p");
-    //     p.textContent=(`${currentValue.date_created}\n ${currentValue.reflectTitle}\n ${currentValue.reflect_message}\n Confidence is ${currentValue.reflect_confidence}`);
-    //     document.getElementById("previously").appendChild(p);
+        p.innerHTML=(`${currentValue.date_created}<br> <strong>${currentValue.reflectTitle}</strong><br> ${currentValue.reflect_message}<br> Confidence is ${currentValue.reflect_confidence}`);
+        // document.getElementById("previously").appendChild(p);
     
 
-    new_list.forEach(function(currentValue){
-        entryBlockDisplay= (`${entryBlockDisplay} ${currentValue.date_created}\n ${currentValue.reflectTitle}\n ${currentValue.reflect_message}\n Confidence is ${currentValue.reflect_confidence}\n`);
+    // new_list.forEach(function(currentValue){
+    //     entryBlockDisplay= (`${entryBlockDisplay} ${currentValue.date_created}\n ${currentValue.reflectTitle}\n ${currentValue.reflect_message}\n Confidence is ${currentValue.reflect_confidence}\n`);
     });
-    previousEntries.innerText = (`${entryBlockDisplay}`);
+    document.getElementById("previously").appendChild(p);
     // for (let index = 0; index < new_list.length; index++) {
         // let prevJournal = document.createElement("p");
         // prevJournal.textConent = (`${new_list[index].date_created}\n ${new_list[index].reflectTitle}\n ${new_list[index].reflect_message}\n Confidence is ${new_list[index].reflect_confidence}`);
@@ -56,7 +58,7 @@ let main = () => {
     // !! not sure the point of the index value
     reflectDate = document.getElementById("date");
     reflectTitle = document.getElementById("title");
-    reflectMessage = document.getElementById("reflection");
+    reflectMessage = document.getElementById("message");
     reflectConfidence = document.getElementById("reflection_confidence");
     submitBtn = document.getElementById("submit");
     submitBtn.addEventListener("click", (evt) => {
