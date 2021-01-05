3b22da9febf4c1e7e23ecc2cc3341928c452ad91
Awhit-commits <ndrw_whtmr@ymail.com>
2021-01-05 09:17:23 -0600

    fixed variables

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
 let printEntries = (new_list) => {
     let previousEntries = document.getElementById("previously")
-    // let entryBlockDisplay ="";
+        // let entryBlockDisplay ="";
     let p = document.createElement("p");
-    
-     // let sum = 0;
-    new_list.forEach(function(currentValue,index){
-    //     let p = document.createElement("p");
-        p.innerHTML=(`${currentValue.date_created}<br> <strong>${currentValue.reflectTitle}</strong><br> ${currentValue.reflect_message}<br> Confidence is ${currentValue.reflect_confidence}`);
-        // document.getElementById("previously").appendChild(p);
-    
 
-    // new_list.forEach(function(currentValue){
-    //     entryBlockDisplay= (`${entryBlockDisplay} ${currentValue.date_created}\n ${currentValue.reflectTitle}\n ${currentValue.reflect_message}\n Confidence is ${currentValue.reflect_confidence}\n`);
+    // let sum = 0;
+    new_list.forEach(function(currentValue, index) {
+        //     let p = document.createElement("p");
+        p.innerHTML = (`${currentValue.reflectDate}<br> <strong>${currentValue.reflectTitle}</strong><br> ${currentValue.reflectMessage}<br> Confidence is ${currentValue.reflectConfidence}`);
+
+
+
+
     });
     document.getElementById("previously").appendChild(p);
     // for (let index = 0; index < new_list.length; index++) {
-        // let prevJournal = document.createElement("p");
-        // prevJournal.textConent = (`${new_list[index].date_created}\n ${new_list[index].reflectTitle}\n ${new_list[index].reflect_message}\n Confidence is ${new_list[index].reflect_confidence}`);
-        // document.getElementById("previously").appendChild(prevJournal);
-        
-        // !! your commented out version is closer to solution, you will need to iterate through the list and it can be achieved without append child - check out my solution in the gist I sent in the slack for a possible approach
-    
+    // let prevJournal = document.createElement("p");
+    // prevJournal.textConent = (`${new_list[index].date_created}\n ${new_list[index].reflectTitle}\n ${new_list[index].reflect_message}\n Confidence is ${new_list[index].reflect_confidence}`);
+    // document.getElementById("previously").appendChild(prevJournal);
+
+    // !! your commented out version is closer to solution, you will need to iterate through the list and it can be achieved without append child - check out my solution in the gist I sent in the slack for a possible approach
+
     console.log(new_list)
-    // let p = document.createElement("p");
-    // p.textContent = `${new_list[index].date_created}`
-    
+        // let p = document.createElement("p");
+        // p.textContent = `${new_list[index].date_created}`
+
     // document.getElementById("previously").appendChild(p);
     // index++;
-    
+
 
 }
 
 
 //Function for creating object instances of journal data
-let createJournal = (date, title, message, confidence,list) => {
+let createJournal = (date, title, message, confidence, list) => {
     // let journalEntries =[];
     const journalEntry = {
-        date_created: date,
-        reflectTitle: title,
-        reflect_message: message,
-        reflect_confidence: confidence,
-    }
-    //Pushing everything to a list
+            reflectDate: date,
+            reflectTitle: title,
+            reflectMessage: message,
+            reflectConfidence: confidence,
+        }
+        //Pushing everything to a list
 
 
     // console.log(journalEntries);
@@ -50,7 +49,7 @@ let createJournal = (date, title, message, confidence,list) => {
     console.log(list);
 
     //Sending list to print to DOM
-    printEntries (list);
+    printEntries(list);
 
 }
 let main = () => {
@@ -71,7 +70,7 @@ let main = () => {
         titleValue = reflectTitle.value;
         messageValue = reflectMessage.value;
         confidenceValue = reflectConfidence.value;
-        createJournal(dateValue, titleValue, messageValue, confidenceValue,journalEntries);
+        createJournal(dateValue, titleValue, messageValue, confidenceValue, journalEntries);
         // evt.preventDefault();
     })
 
